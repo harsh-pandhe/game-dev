a09a53b5d4b65572f197f78e86b32458d4850532
G4mingBoy <arifmohammed260@gmail.com>
2023-08-28 22:39:15 +0530

    TitleMusicAdded.

--- Island Odyssey_ Quest for Enigma's Treasure/Scripts/BackgroundSounds.gd
+++ Island Odyssey_ Quest for Enigma's Treasure/Scripts/BackgroundSounds.gd
@@ -1,7 +1,7 @@
 extends AudioStreamPlayer
 
 func _ready() -> void:
-	var audioStream: AudioStream = preload("res://Assets/Sounds/Background/BackgroundSamp.mp3")
+	var audioStream: AudioStream = preload("res://Assets/Sounds/Background/MainTitle.mp3")
 	self.set_stream(audioStream)
 	self.set_volume_db(-20)
 	self.play()
